f81fe4685512f0995bc78148caa6ea29924ab038
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2019-02-11 13:36:32 -0800

    sass folder

--- client/src/component/Pages/Journal.js
+++ client/src/component/Pages/Journal.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {Row, Col, Icon,Button, Modal, Input } from "react-materialize";
+import { Row, Col, Icon, Button, Modal, Input } from "react-materialize";
 import "./home.css";
 
 class Journal extends React.Component {
     state = {
-        
+
     };
 
 
@@ -12,41 +12,48 @@ class Journal extends React.Component {
         return (
             <div>
                 <Row>
-                <Col s={3} className='grid-example'></Col>
-                <Col s={3} className='grid-example'>
+                    <Col s={3} className='grid-example'></Col>
+                    <Col s={3} className='grid-example'>
 
-                    <h2>My trip to the fridge. <span id="editBtn"><Icon>edit</Icon></span></h2>
-                    <hr />
-                    <p>Quis occaecat consequat quis pariatur reprehenderit. Laboris nulla non irure sint et irure do commodo. Enim proident ex ut non do adipisicing reprehenderit laboris veniam exercitation ad eu deserunt et.
+                        <h2>My trip to the fridge. <span id="editBtn"><Icon>edit</Icon></span></h2>
+                        <hr />
+                        <p>Quis occaecat consequat quis pariatur reprehenderit. Laboris nulla non irure sint et irure do commodo. Enim proident ex ut non do adipisicing reprehenderit laboris veniam exercitation ad eu deserunt et.
                         <br /><br />
-                        Exercitation cupidatat ad laboris voluptate dolor ex eiusmod. Qui eiusmod velit eu est deserunt duis consequat. Enim est cupidatat cupidatat commodo sunt laborum sint.
+                            Exercitation cupidatat ad laboris voluptate dolor ex eiusmod. Qui eiusmod velit eu est deserunt duis consequat. Enim est cupidatat cupidatat commodo sunt laborum sint.
                         <br /><br />
-                        Qui dolor nostrud sunt occaecat dolor commodo consequat exercitation voluptate Lorem. Sint laborum elit ut voluptate in anim cillum duis adipisicing consequat amet adipisicing et ex. Magna occaecat est nulla nostrud tempor nisi sint cillum est eu mollit do.</p>
-                        <Button>Edit</Button>                        
+                            Qui dolor nostrud sunt occaecat dolor commodo consequat exercitation voluptate Lorem. Sint laborum elit ut voluptate in anim cillum duis adipisicing consequat amet adipisicing et ex. Magna occaecat est nulla nostrud tempor nisi sint cillum est eu mollit do.</p>
+                            <Modal
+                            header='Modal Header'
+                            trigger={<Button waves='light'>Edit<Icon right>insert_chart</Icon></Button>}>
+                            <Input type='title' />
 
-                        </Col>
-                        
-                        
-                        <Col s={4} className='journal_pics'>
-                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper"/>
-                        <img className="fit_img" src="https://via.placeholder.com/300x150/666.png/fff" alt="tripper"/>
-                        <img className="fit_img" src="https://via.placeholder.com/150x300/666.png/fff" alt="tripper"/>
-                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper"/>
-                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper"/>
-                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper"/>
-                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper"/>
+                            <Input type='textarea' />
+
+                        </Modal>
+
+                    </Col>
+
+
+                    <Col s={4} className='journal_pics'>
+                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper" />
+                        <img className="fit_img" src="https://via.placeholder.com/300x150/666.png/fff" alt="tripper" />
+                        <img className="fit_img" src="https://via.placeholder.com/150x300/666.png/fff" alt="tripper" />
+                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper" />
+                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper" />
+                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper" />
+                        <img className="fit_img" src="https://via.placeholder.com/150/666.png/fff" alt="tripper" />
                         <Modal
-  header='Modal Header'
-  trigger={<Button waves='light'>Add<Icon right>insert_chart</Icon></Button>}>
-      <Input type='title' />
+                            header='Modal Header'
+                            trigger={<Button waves='light'>Add<Icon right>insert_chart</Icon></Button>}>
+                            <Input type='title' />
 
-    <Input type='textarea' />
+                            <Input type='textarea' />
 
-</Modal>
+                        </Modal>
 
-                        </Col>
-                        <Col s={2} className='grid-example'></Col>
-                    </Row>
+                    </Col>
+                    <Col s={2} className='grid-example'></Col>
+                </Row>
             </div>
         );
     }
