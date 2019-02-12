7a90497c0d4bb890a847c088fd02c26e5e4f5de7
Steven Brown <stevendbrown08@gmail.com>
2019-02-12 10:18:52 -0800

    small changes to Map.js; first draft of Container.js

--- client/src/component/Map/Map.js
+++ client/src/component/Map/Map.js
@@ -93,10 +93,25 @@ export class Map extends React.Component {
         }
     }
 
+    renderChildren(){
+        const {children} = this.props;
+
+        if(!children) return;
+
+        return React.Children.map(children, c =>{
+            return React.cloneElement(c, {
+                map: this.map,
+                google: this.props.google,
+                mapCenter: this.state.currentLocation
+            });
+        });
+    }
+
     render() {
         return (
             <div ref='map'>
                 Loading map...
+                {this.renderChildren()}
         </div>
         )
     }
