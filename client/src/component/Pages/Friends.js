63fad47a1f166220882e42a42506f0d44361d022
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2019-02-12 13:38:48 -0800

    insignificant changes

--- client/src/component/Pages/Friends.js
+++ client/src/component/Pages/Friends.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {Row, Col} from "react-materialize";
+import { Row, Col, Card, CardTitle } from "react-materialize";
 
 class Friends extends React.Component {
     state = {
         user: null
     };
-    
+
     componentDidMount() {
-    
+
     }
+
 
     render() {
         return (
@@ -16,46 +17,49 @@ class Friends extends React.Component {
                 <Row className="FriendProfile">
                     <Col s={1} className='grid-example'></Col>
                     <Col s={3} className='grid-example'>
-                    <div className="SearchCard">
-                    <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
-                    <h3>Mr. Nobody</h3>
-                    <p>CEO & Director</p>
-                    </div></Col>
+                        <Card className='small'
+                            header={<CardTitle image='img/sample-1.jpg'>Card Title</CardTitle>}
+                            actions={[<a href='#'>This is a Link</a>]}>
+                            <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
+                            <h3>Mr. Nobody</h3>
+                            <p>CEO & Director</p>
+                        </Card >
+                    </Col>
                     <Col s={3} className='grid-example'>
-                    <div className="SearchCard">
-                    <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
-                    <h3>Mr. Nobody</h3>
-                    <p>CEO & Director</p>
-                    </div></Col>
+                        <div className="SearchCard">
+                            <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
+                            <h3>Mr. Nobody</h3>
+                            <p>CEO & Director</p>
+                        </div></Col>
                     <Col s={3} className='grid-example'>
-                    <div className="SearchCard">
-                    <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
-                    <h3>Mr. Nobody</h3>
-                    <p>CEO & Director</p>
-                    </div>
+                        <div className="SearchCard">
+                            <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
+                            <h3>Mr. Nobody</h3>
+                            <p>CEO & Director</p>
+                        </div>
                     </Col>
                     <Col s={1} className='grid-example'></Col>
                 </Row>
                 <Row className="FriendProfile">
                     <Col s={1} className='grid-example'></Col>
                     <Col s={3} className='grid-example'>
-                    <div className="SearchCard">
-                    <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
-                    <h3>Mr. Nobody</h3>
-                    <p>CEO & Director</p>
-                    </div></Col>
+                        <div className="SearchCard">
+                            <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
+                            <h3>Mr. Nobody</h3>
+                            <p>CEO & Director</p>
+                        </div></Col>
                     <Col s={3} className='grid-example'>
-                    <div className="SearchCard">
-                    <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
-                    <h3>Mr. Nobody</h3>
-                    <p>CEO & Director</p>
-                    </div></Col>
+                        <div className="SearchCard">
+                            <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
+                            <h3>Mr. Nobody</h3>
+                            <p>CEO & Director</p>
+                        </div></Col>
                     <Col s={3} className='grid-example'>
-                    <div className="SearchCard">
-                    <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
-                    <h3>Mr. Nobody</h3>
-                    <p>CEO & Director</p>
-                    </div>
+                        <div className="SearchCard">
+                            <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
+                            <h3>Mr. Nobody</h3>
+                            <p>CEO & Director</p>
+                        </div>
                     </Col>
                     <Col s={1} className='grid-example'></Col>
                 </Row>
