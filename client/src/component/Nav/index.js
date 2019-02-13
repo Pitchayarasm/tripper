e9651ee9087a2e87d3aaa144e67bf2f868679d20
Steven Brown <stevendbrown08@gmail.com>
2019-02-12 22:39:36 -0800

    removed broken return statement from Nav/index.js

--- client/src/component/Nav/index.js
+++ client/src/component/Nav/index.js
@@ -17,15 +17,15 @@ class Nav extends React.Component {
     friendsPage = () => {
         window.location.href = "/friends"
     }
-    
+
     handleLogout = () => {
-      axios.get("/logout")
-      .then( (res) => {
-        this.props.setUser(res.data);
-        window.location.href = "/"
-      })
+        axios.get("/logout")
+            .then((res) => {
+                this.props.setUser(res.data);
+                window.location.href = "/"
+            })
     }
-    
+
     render() {
 
         let navbar;
@@ -33,43 +33,43 @@ class Nav extends React.Component {
         if (this.props.user) {
             navbar = (
                 <>
-                <Navbar brand='tripper' right>
-                    <NavLink to="" className="tooltipped" data-position="left" data-tooltip="Quick Search"><Icon className="navIcon">search</Icon></NavLink>
-                    <NavLink to="" data-activates="sidenav_3" className="tooltipped" data-position="left" data-tooltip="Chat"><Icon className="navIcon">chat</Icon></NavLink>
-                    <NavLink to="" className="tooltipped" data-position="left" data-tooltip="Notifications"><Icon className="navIcon">notifications</Icon></NavLink>
-                    <NavLink to="/Profile" className="tooltipped" data-position="left" data-tooltip="View Profile"><Icon className="navIcon">account_circle</Icon></NavLink>
-                </Navbar>
+                    <Navbar brand='tripper' right>
+                        <NavLink to="" className="tooltipped" data-position="left" data-tooltip="Quick Search"><Icon className="navIcon">search</Icon></NavLink>
+                        <NavLink to="" data-activates="sidenav_3" className="tooltipped" data-position="left" data-tooltip="Chat"><Icon className="navIcon">chat</Icon></NavLink>
+                        <NavLink to="" className="tooltipped" data-position="left" data-tooltip="Notifications"><Icon className="navIcon">notifications</Icon></NavLink>
+                        <NavLink to="/Profile" className="tooltipped" data-position="left" data-tooltip="View Profile"><Icon className="navIcon">account_circle</Icon></NavLink>
+                    </Navbar>
 
-                <Button floating fab="horizontal" icon="navigation" className="red" large style={{bottom: "45px", right: "24px"}}>
-                    <Button floating icon="add" className="blue darken-4" data-position="top" tooltip="Create Journal"/>
-                    <Button floating icon="group" className="yellow darken-3" data-position="top" onClick={this.friendsPage} tooltip="Friends"/>
-                    <Button floating icon="power_settings_new" className="red darken-1" onClick={this.handleLogout} data-position="top" tooltip="Logout"/>
-                </Button>
+                    <Button floating fab="horizontal" icon="navigation" className="red" large style={{ bottom: "45px", right: "24px" }}>
+                        <Button floating icon="add" className="blue darken-4" data-position="top" tooltip="Create Journal" />
+                        <Button floating icon="group" className="yellow darken-3" data-position="top" onClick={this.friendsPage} tooltip="Friends" />
+                        <Button floating icon="power_settings_new" className="red darken-1" onClick={this.handleLogout} data-position="top" tooltip="Logout" />
+                    </Button>
                 </>
             );
         }
-         else {
-             navbar = (
-                 <Navbar id="navBarOut" brand='tripper' right></Navbar>
-             );
-         }
+        else {
+            navbar = (
+                <Navbar id="navBarOut" brand='tripper' right></Navbar>
+            );
+        }
 
         return (
             <>
                 {navbar}
 
                 <SideNav
-                    trigger={<span style={{display: "none"}}></span>}
+                    trigger={<span style={{ display: "none" }}></span>}
                     options={{ closeOnClick: true, edge: "right" }}
                     data-sidenav="mySideNav"
                 >
                     <SideNavItem
-                    userView
-                    user={{
-                        background: "",
-                        image: "https://www.thefamouspeople.com/profiles/images/anderson-cooper-5.jpg",
-                        name: "Anderson Cooper",
-                        email: "andersoncooper@cnn.com"
+                        userView
+                        user={{
+                            background: "",
+                            image: "https://www.thefamouspeople.com/profiles/images/anderson-cooper-5.jpg",
+                            name: "Anderson Cooper",
+                            email: "andersoncooper@cnn.com"
                         }}
                     />
                     <SideNavItem className="onlineHeader"><Icon className="icon-online">rss_feed</Icon>Online Friends</SideNavItem>
@@ -81,33 +81,6 @@ class Nav extends React.Component {
             </>
         );
     }
-
-    return (
-      <>
-        {navbar}
-
-        <SideNav
-          trigger={<span style={{ display: "none" }}></span>}
-          options={{ closeOnClick: true, edge: "right" }}
-        >
-          <SideNavItem
-            userView
-            user={{
-              background: "",
-              image: "https://www.thefamouspeople.com/profiles/images/anderson-cooper-5.jpg",
-              name: "Anderson Cooper",
-              email: "andersoncooper@cnn.com"
-            }}
-          />
-          <SideNavItem className="onlineHeader"><Icon className="icon-online">rss_feed</Icon>Online Friends</SideNavItem>
-          <SideNavItem className="onlineFriend" onClick={() => this.props.startChat(true)}>Chris Cuomo <Icon className="icon-friend-online">lens</Icon></SideNavItem>
-          <SideNavItem divider />
-          <SideNavItem subheader>Offline Friends</SideNavItem>
-          {/* <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>  */}
-        </SideNav>
-      </>
-    );
-  }
 }
 
 export default Nav;
