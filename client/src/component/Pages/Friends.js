e516413463d89ed55b5607c129e86ce927e847ab
Steven Brown <stevendbrown08@gmail.com>
2019-02-13 13:55:21 -0800

    fixed minor issues

--- client/src/component/Pages/Friends.js
+++ client/src/component/Pages/Friends.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Card, CardTitle } from "react-materialize";
+import { Row, Col } from "react-materialize";
 import axios from "axios";
 
 class Friends extends React.Component {
@@ -18,11 +18,11 @@ class Friends extends React.Component {
                 journals: [0]
             }
         ],
-        friendsHTML: <></>
+        friendsHTML: []
     };
 
     componentDidMount() {
-        /*axios.get({ user: this.props.user._id }).friends;} */
+ /*axios.get({ user: this.props.user._id }).friends;} */
         this.loadFriends();
     }
 
@@ -43,7 +43,7 @@ class Friends extends React.Component {
 
         this.setState({ friendsHTML: html });
     }
-
+    
     generateRow(i, friendList) {
         let html = [];
         for (let j = 0; j < 3 && i * 3 + j < friendList.length; j++) {
@@ -110,7 +110,7 @@ class Friends extends React.Component {
                     </Col>
                     <Col s={1} className='grid-example'></Col>
                 </Row> */}
-            </>
+            </> 
         );
     }
 }
