8a27dcebbd04fe7f83ff29476c84e9ec618fb099
Thanyah Kung <pitchayarasm.kunghae@hotmail.com>
2019-02-14 13:12:13 -0800

    fix journal

--- client/src/component/Pages/Journal.js
+++ client/src/component/Pages/Journal.js
@@ -3,34 +3,12 @@ import { Row, Col, Icon, Button, Modal, Input } from "react-materialize";
 import axios from "axios"
 
 class Journal extends React.Component {
-<<<<<<< HEAD
     state = {
         entryTitle : "",
         entryText : "",
         entryLocation : "",
         entries : []
     };
-=======
-    constructor(props) {
-        super(props);
-        this.state = {
-            entryTitle: "",
-            entryText: "",
-            entryLocation: ""
-        };
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (this.props.user._id === undefined && nextProps.user._id) {
-            this.axiosCall(nextProps.user._id);
-        }
-    }
-
-    axiosCall = id => {
-        axios.get(`friendList/${id}`)
-            .then(res => console.log(res.data));
-    }
->>>>>>> frontEndCalls
 
     componentWillReceiveProps(nextProps) {
         if ( this.props.user._id === undefined && nextProps.user.journals) {
@@ -63,7 +41,6 @@ class Journal extends React.Component {
 
     handleSubmit = () => {
         axios.post(`entry/${this.props.user.journals}`
-<<<<<<< HEAD
         ,{
             title: this.state.entryTitle,
             body: this.state.entryText,
@@ -84,52 +61,12 @@ class Journal extends React.Component {
                 });
             }
         });
-=======
-            , {
-                title: this.state.entryTitle,
-                body: this.state.entryText,
-                location: this.state.entryLocation
-            })
-            .then(res => {
-                if (res) {
-                    console.log(res.data)
-                    this.setState({
-                        // front-end grab data here
-                    })
-                }
-            })
->>>>>>> frontEndCalls
     }
-
-    // generateEntries() {
-    //     for (let i = 0; i < this.props.user.journals)
-    // }
 
     render() {
         let entry;
 
-<<<<<<< HEAD
         if (this.state.entries) {
-=======
-        if (true/*this.props.user.journals.name*/) {
-            entry = (
-                <>
-                    <Row>
-                        <Col s={3} className='grid-example'></Col>
-                        <Col s={3} className='grid-example'>
-                            {/* <h2>{this.props.journals.name} <span id="editBtn"><Icon>edit</Icon></span></h2> */}
-                            <hr />
-                        </Col>
-                        <Col s={4} className='journal_pics'>
-                            {this.generateEntries}
-                        </Col>
-                        <Col s={2} className='grid-example'></Col>
-                    </Row>
-                </>
-            );
-        }
-        else {
->>>>>>> frontEndCalls
             entry = (
                 <> 
                 { this.state.entries.map(entry => ( 
@@ -147,7 +84,6 @@ class Journal extends React.Component {
                     </Row>
                 ))}
                 <Modal
-<<<<<<< HEAD
                 header='Add your Entry!'
                 trigger={<Button className="homeBtn pulse">Add Entry</Button>}
                 actions={<><Button className="cancel modal-action modal-close">Cancel</Button><Button id="signUpSubmit" className="cancel modal-action modal-close" onClick={this.handleSubmit}>Submit</Button></>}
@@ -164,21 +100,6 @@ class Journal extends React.Component {
         }
          else {
          }
-=======
-                    header='Add your Entry!'
-                    trigger={<Button className="homeBtn">Add Entry</Button>}
-                    actions={<><Button className="cancel modal-action modal-close">Cancel</Button><Button id="signUpSubmit" className="cancel modal-action modal-close" onClick={this.handleSubmit}>Submit</Button></>}
-                >
-                    <Row id="addNewEntryForm">
-                        <Input type='title' id="entryTitle" label="Title" value={this.state.entryTitle} onChange={this.handleInputChange} />
-                        <Input type='textarea' id="entryText" label="Body" value={this.state.entryText} onChange={this.handleInputChange} />
-                        <Input type='textarea' id="entryLocation" label="location" value={this.state.entryLocation} onChange={this.handleInputChange} />
-                        <Input name="profileImg" type="file" id="profileImg" label="Upload" placeholder="no file choosen" s={12} />
-                    </Row>
-                </Modal>
-            );
-        }
->>>>>>> frontEndCalls
         return (
             <>
                 {entry}
