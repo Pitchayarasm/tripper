4567e7e736e446d8a5cbb312d9f97cdd56c98b89
Thanyah Kung <pitchayarasm.kunghae@hotmail.com>
2019-02-14 14:11:55 -0800

    add about me

--- client/src/component/Pages/Profile.js
+++ client/src/component/Pages/Profile.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { Row, Col, Input, Modal, Button } from "react-materialize";
+import { Row, Col, Input, Modal, Button,Icon } from "react-materialize";
 import axios from "axios";
 
 class Profile extends React.Component {
     state = {
+        about_me : "",
+        entry : ""
     };
+
+    componentWillReceiveProps(nextProps) {
+        if ( this.props.user._id === undefined && nextProps.user.journals) {
+            this.getEntry(nextProps.user.journals);
+        }
+    }
+
+    getEntry = (journalId) => {
+        axios.get(`/entry/${journalId}`)
+        .then( res => {
+            console.log(res.data.entries)
+            if ( res ) {
+                this.setState({
+                    entry : res.data.entries[res.data.entries.length-1]
+                })
+            }
+        })
+        .catch( err => {
+            console.log(err);
+        }) 
+    }
 
     handleselectedFile = () => {
         let formData = new FormData();
@@ -21,20 +44,56 @@ class Profile extends React.Component {
         });
     }
 
-    render() {
-        let profile = <Modal
-            trigger={<Button className="homeBtn">Add Profile</Button>}
-            actions={<><Button className="cancel modal-action modal-close">Cancel</Button><Button id="signUpSubmit" className="cancel modal-action modal-close" onClick={this.handleselectedFile}>Submit</Button></>}
-        >
-            <Row id="signUpForm">
-                <Input name="profileImg" type="file" id="profileImg" label="Upload" placeholder="no file choosen" s={12} />
-            </Row>
-        </Modal>;
+    handleInputChange = event => {
+        this.setState({
+            about_me : event.target.value
+        });
+    };
 
+    handleSubmit = () => {
+        axios.post(`aboutme/${this.props.user._id}`
+        ,{
+            about_me : this.state.about_me
+        })
+        .then( res => {
+            console.log(res.data)
+            this.props.setUser(res.data)
+        });
+    }
+
+    render() {
+        let profile;
         if (this.props.user.file) {
             let src = `/upload/${this.props.user.file}`
             profile =  <img style={{width:"100%"}} src={src} alt="profile" />
         }
+
+        let about_me;
+        if (this.props.user.about_me) {
+            about_me = <p>{this.props.user.about_me}</p>
+        }
+
+        let entry;
+        if (this.state.entry) {
+            entry = <Row>
+                    <Col s={3} className='grid-example'></Col>
+                    <Col s={3} className='grid-example'>
+                        <h2>{this.state.entry.title}</h2>
+                        <hr />
+                        <h5>{this.state.entry.body}</h5>
+                        <p><Icon>location_on</Icon>{this.state.entry.location}</p>
+                    </Col>
+                    
+                    <Col s={2} className='grid-example'></Col>
+                    </Row>
+        }
+        let entry_pic;
+        if (this.state.entry.file) {
+            entry_pic = <Col s={4} className='journal_pics'>
+                        <img className="fit_img tile" src={`upload/${this.state.entry.file}`} alt="tripper" />
+                        </Col>
+        }
+
 
         return (
             <div>
@@ -42,25 +101,33 @@ class Profile extends React.Component {
                     <Col s={2} className='grid-example'></Col>
                     <Col s={4} className='grid-example'>
                         {profile}
-                        <div className="textBox">
-                            <h2>The moon is great!</h2>
-                            <p>Quis occaecat consequat quis pariatur reprehenderit. Laboris nulla non irure sint et irure do commodo. Enim proident ex ut non do adipisicing reprehenderit laboris veniam exercitation ad eu deserunt et.</p>
-                        </div>
+                        <Modal
+                        trigger={<Button className="homeBtn">Add Profile</Button>}
+                        actions={<><Button className="cancel modal-action modal-close">Cancel</Button><Button id="signUpSubmit" className="cancel modal-action modal-close" onClick={this.handleselectedFile}>Submit</Button></>}
+                        >
+                        <Row id="signUpForm">
+                            <Input name="profileImg" type="file" id="profileImg" label="Upload" placeholder="no file choosen" s={12} />
+                        </Row>
+                        </Modal>
+                        {entry}
                     </Col>
 
 
                     <Col s={3} className='grid-example'>
                         <div className="textBox">
                             <h2>{this.props.user.firstName} {this.props.user.lastName}</h2>
-                            <p>Quis occaecat consequat quis pariatur reprehenderit. Laboris nulla non irure sint et irure do commodo. Enim proident ex ut non do adipisicing reprehenderit laboris veniam exercitation ad eu deserunt et.</p>
+                            {about_me}
                             <Modal
-                                header='About me'
-                                trigger={<Button>Add</Button>}>
-                                <Input type='textarea' />
+                            header='Add your story!'
+                            trigger={<Button className="homeBtn pulse"><Icon>edit</Icon></Button>}
+                            actions={<><Button className="cancel modal-action modal-close">Cancel</Button><Button id="signUpSubmit" className="cancel modal-action modal-close" onClick={this.handleSubmit}>Submit</Button></>}
+                            > 
+                            <Row id="addNewEntryForm">
+                                <Input type='textarea' id="entryTitle" label="About me.." value={this.state.about_me} onChange={this.handleInputChange} />
+                            </Row>
                             </Modal>
                         </div>
-                        <img className="Friend" src="https://via.placeholder.com/346/666.png/fff" alt="tripper" />
-
+                        {entry_pic}
                     </Col>
                     <Col s={3} className="grid-example"></Col>
                 </Row>
